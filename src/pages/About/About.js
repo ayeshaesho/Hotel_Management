040e348740f0567b5a230b3b9a7d57d8ad8901f8
ayeshaphero <ayeshatalukderctg4656@gmail.com>
2022-12-27 22:11:41 +0600

    home

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import './About.css'
-// import profile from '../../image/profile/Ayesha_Esho.JPG';
+import React from 'react';
+import './About.css'
+import profile from '../../image/profile/Ayesha_Esho.JPG';
 
-// const About = () => {
-// 	return (
-// 		<div className='container about'>
-// 			<h2 className='text-center mt-5'>About</h2>
-// 			<div style={{height:'3px', width:'40px'}} className='bg-warning fw-bold my-2 mx-auto mb-5'></div>
-// 			<div className='blogs-question about-info'>
-// 				<div className='about-text'>
-// 					<h5 style={{textAlign:'justify'}}>My Name Is Ayesha Esho.I Am Study in BGC Trust University in CSE. and I Start My Internship at NXgit IT Farm This Month! My Dream Job Is To Become a Successful Software Engineer.</h5>
-// 				</div>
-// 				<div className='d-flex justify-content-end about-img'>
-// 					<img height={180} src={profile} alt="" />
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
+const About = () => {
+	return (
+		<div className='container about'>
+			<h2 className='text-center mt-5'>About</h2>
+			<div style={{height:'3px', width:'40px'}} className='bg-warning fw-bold my-2 mx-auto mb-5'></div>
+			<div className='blogs-question about-info'>
+				<div className='about-text'>
+					<h5 style={{textAlign:'justify'}}>My Name Is Ayesha Esho.I Am Study in BGC Trust University in CSE. and I Start My Internship at NXgit IT Farm This Month! My Dream Job Is To Become a Successful Software Engineer.</h5>
+				</div>
+				<div className='d-flex justify-content-end about-img'>
+					<img height={180} src={profile} alt="" />
+				</div>
+			</div>
+		</div>
+	);
+};
 
-// export default About;
+export default About;
