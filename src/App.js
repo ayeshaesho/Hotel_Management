fecb9914b7e53465a453744cb3beac7e7a7b1d85
ayeshaphero <ayeshatalukderctg4656@gmail.com>
2022-12-27 22:10:52 +0600

    index

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Route, Routes } from 'react-router-dom';
-// import './App.css';
-// import About from './pages/About/About';
-// import Blog from './pages/Blog/Blog';
-// import Checkout from './pages/Checkout/Checkout';
-// import Home from './pages/Home/Home/Home';
-// import Login from './pages/LoginSignup/Login/Login';
-// import RequireAuth from './pages/LoginSignup/RequireAuth/RequireAuth';
-// import Signup from './pages/LoginSignup/Signup/Signup';
-// import Footer from './pages/Shared/Footer/Footer';
-// import Header from './pages/Shared/Header/Header';
-// import NotFound from './pages/Shared/NotFound/NotFound';
-// import Thanks from './pages/Shared/Thanks/Thanks';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './pages/About/About';
+import Blog from './pages/Blog/Blog';
+import Checkout from './pages/Checkout/Checkout';
+import Home from './pages/Home/Home/Home';
+import Login from './pages/LoginSignup/Login/Login';
+import RequireAuth from './pages/LoginSignup/RequireAuth/RequireAuth';
+import Signup from './pages/LoginSignup/Signup/Signup';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
+import NotFound from './pages/Shared/NotFound/NotFound';
+import Thanks from './pages/Shared/Thanks/Thanks';
 
-// function App() {
-//   return (
-//     <div>
-//       <Header></Header>
-//       <Routes>
-//         <Route path='/' element={<Home></Home>}></Route>
-//         <Route path='/home' element={<Home></Home>}></Route>
-//         <Route path='/login' element={<Login></Login>}></Route>
-//         <Route path='/signup' element={<Signup></Signup>}></Route>
-//         <Route path='/blogs' element={<Blog></Blog>}></Route>
-//         <Route path='/about' element={<About></About>}></Route>
-//         <Route  path='/course/:courseId' element={
-//           <RequireAuth>
-//             <Checkout></Checkout>
-//           </RequireAuth>
-//         }></Route>
-//         <Route path='/thanks' element={<Thanks></Thanks>}></Route>
-//         <Route path='*' element={<NotFound></NotFound>}></Route>
-//       </Routes>
-//      <Footer></Footer>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/blogs' element={<Blog></Blog>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route  path='/course/:courseId' element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
+        <Route path='/thanks' element={<Thanks></Thanks>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+     <Footer></Footer>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
